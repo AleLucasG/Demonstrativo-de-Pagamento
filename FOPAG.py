d77b416bdd10182134f92fb67e8ceb67342984f5
Alessandra Lucas <alessandra1702@hotmail.com>
2022-01-28 19:11:19 -0300

    Create FOPAG.py
    Ajuste adto quinzenal

---
+++ FOPAG.py
@@ -0,0 +1,55 @@
+"""Demonstrativo Mensal de Pagamento"""
+
+print('\033[1;32m========= HOLERITE =========')
+print('=== PAGAMENTO MENSALISTA === \033[m\n')
+
+"""DADOS INICIAIS"""
+nome = str(input('Funcionario: ')).strip()
+salarioBaseMes = float(input('Salario base: '))
+diasTrabMes: int = int(input('Dias trabalhado mês: '))
+dependente = int(input('Quant. Dependenete IRRF: '))
+depenSalarioFam = int(input('Quant. Dependnete Salário Fámilia:  '))
+print('\n')
+
+"""ADIANTAMENTO"""
+print('\033[1;32mPagamento adiantamento quinzenal - 40% salário base \033[m')
+adto = str(input('Recebe adiantamento quinzenal [SIM/NÃO]: ')).upper()
+if adto == 'SIM' and diasTrabMes <= 14:
+    print('\033[1;31mNão recebe adto quinzenal, dias trabalhados insuficiente para pagamento \033[m')
+else:
+      if adto == 'SIM' and diasTrabMes >= 15:
+        valor = (salarioBaseMes / 30) * diasTrabMes
+        print('Adiantamento quinzenal R$ {}'.format(valor))
+      else:
+          if adto == 'NÃO':
+              print('\033[1;31mNão tem pgto adto quinzenal \033[m')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
