83774d4f1fdde54057864e10f707bde6d6ac0fc7
Alessandra Lucas <alessandra1702@hotmail.com>
2022-02-13 20:53:12 -0300

    Update FOPAG.py

--- FOPAG.py
+++ FOPAG.py
@@ -7,8 +7,6 @@ print('=== PAGAMENTO MENSALISTA === \033[m\n')
 nome = str(input('Funcionario: ')).strip()
 salarioBaseMes = float(input('Salario base: '))
 diasTrabMes: int = int(input('Dias trabalhado mês: '))
-dependente = int(input('Quant. Dependenete IRRF: '))
-depenSalarioFam = int(input('Quant. Dependente Salário Fámilia até 14 anos: '))
 print('\n')
 
 """ADIANTAMENTO"""
@@ -21,32 +19,47 @@ elif adto == 'SIM' and diasTrabMes >= 15:
     print('Adiantamento quinzenal R$ {:.2f}'.format(valor))
 elif adto == 'NÃO':
     print('\033[1;31mNão tem pgto adto quinzenal \033[m')
+else:
+    print('\033[1;31mOPÇÃO ERRADA. SEM CALCCULO DE ADTO SALÁRIO.')
 print('\n')
 
-print('\033[1;32mSALARIO FAMILIA: Valor pago para remunerações até R$ 1503,25. Cota por filho R$ 51,27 \033[m')
+
+print('\033[1;32mSALARIO FAMILIA:\033[m \033[1;31mValor pago para remunerações até R$ 1503,25. Cota por filho R$ 51,27 \033[m')
 if salarioBaseMes <= 1503.25:
     salarioFam = depenSalarioFam * 51.27
     print('Salario Familia: {:.2f}'.format(salarioFam))
 elif salarioBaseMes >= 1503.26:
-    print('\033[1;32mNão recebe salario familia \033[m')
-print('\n')
+    print('\033[1;32mNão recebe salario familia \n \033[m')
 
 """INSS"""
+inss = float
 print('\033[1;32mDedução INSS\033[m')
-if salarioBaseMes >= 0 and salarioBaseMes <= 1100.00:
+if 0 <= salarioBaseMes <= 1100.00:
     inss = (salarioBaseMes * 7.5) / 100
 
-elif salarioBaseMes >= 1100.01 and salarioBaseMes <= 2203.48:
+elif 1100.01 <= salarioBaseMes <= 2203.48:
     inss = (((salarioBaseMes - 1100.00) * 9) / 100) + 82.50
 
-elif salarioBaseMes >= 2203.49 and salarioBaseMes <= 3305.22:
+elif 2203.49 <= salarioBaseMes <= 3305.22:
     inss = (((salarioBaseMes - 2203.48) * 12) / 100) + 181.81
 
-elif salarioBaseMes >= 3305.23 and salarioBaseMes <= 6433.57:
+elif 3305.23 <= salarioBaseMes <= 6433.57:
     inss = (((salarioBaseMes - 3305.23) * 14) / 100) + 314.02
 
 elif salarioBaseMes >= 6433.58:
     inss = 751.99
+
 print('Desconto do INSS é de R$ {:.2f}'.format(inss))
+liquido:float = salarioBaseMes - inss
+print('Liquido R$ {:.2f} \n'.format(liquido))
 
-
+"""IR"""
+ir = float
+liquido1 = float
+print('\033[1;32mDedução IR\033[m')
+dependente = int(input('Quantos dependetes de IR: '))
+if dependente >= 1:
+    ir = dependente * 189.59
+    print('Dedução IR por {} dependente é de R$ {}'.format(dependente, ir))
+    liquido1 = liquido - ir
+    print('Liquido R$ {:.2f}'.format(liquido1))
